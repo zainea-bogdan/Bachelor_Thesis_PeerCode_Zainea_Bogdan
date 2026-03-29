dac6a58d39217f67bd9c1a2aaae95da12cc3f900
bodoz <118077974+zainea-bogdan@users.noreply.github.com>
2026-03-29 22:47:46 +0300

    DONE: finished the commit analysis v1 with metrics resume and some basic flags

--- git_analysis/routers/metrics.py
+++ git_analysis/routers/metrics.py
@@ -21,6 +21,7 @@ def get_commits_metrics(github_username:str,
         }
 
 
+
     try:
         repo = github_repo
         category_of_metrics = "commits_timeline_analysis"
@@ -39,7 +40,9 @@ def get_commits_metrics(github_username:str,
         #calculating each metric:
         metrics_data={}
         metrics_data["total_number_of_real_commits"]=len(non_merge_commits_list)
-
+        metrics_data["total_number_of_merge_commits"]=commits_data.get("count")-len(non_merge_commits_list)
+        metrics_data["ratio_real_commits"]= metrics_data["total_number_of_real_commits"]/( metrics_data["total_number_of_real_commits"]+metrics_data["total_number_of_merge_commits"])
+       
         #extracting unique dates form the commits:
         #example of how the unique date values look like (not ai generated this comment :) )
         """
@@ -60,9 +63,25 @@ def get_commits_metrics(github_username:str,
 
         #avg commits per day:
         metrics_data["avg_number_of_real_commits_per_day"]=metrics_data["total_number_of_real_commits"]/ metrics_data["active_days"]
+
+        #calculating the biggest gap of active days.
+        max_days_gap=0
+        list_days_gaps=[]
+        list_dates=sorted(list(unique_date_values))
+        for i in range(len(list_dates)):
+            if i!=0:
+                list_days_gaps.append((list_dates[i]-list_dates[i-1]).days )
+                if (list_dates[i]-list_dates[i-1]).days > max_days_gap:
+                    max_days_gap=(list_dates[i]-list_dates[i-1]).days
+            else:
+                continue
         
+        metrics_data["largest_inactivity_gap_days"]=max_days_gap
+        metrics_data["days_gaps_sorted"]=list_days_gaps
+
         #ratio of commits in last days.
         counter_last_day_commits=0
+        commits_per_day = {}
         deadline_data_ajustata = datetime.strptime(deadline_data, "%Y-%m-%d").date()
         for com in non_merge_commits_list:
             if com.get("commit_committer_date"):
@@ -70,15 +89,34 @@ def get_commits_metrics(github_username:str,
                     com.get("commit_committer_date"),
                     "%Y-%m-%dT%H:%M:%SZ"
                 ).date()
-
+            # for deadline calculation
             if commit_date == deadline_data_ajustata:
                     counter_last_day_commits += 1
+            #for max day commits ratio
+            if commit_date in commits_per_day:
+                commits_per_day[f"{commit_date}"]+=1
+            else:
+                commits_per_day[f"{commit_date}"]=1
 
-        metrics_data["last_day_commit_ratio"]=counter_last_day_commits/metrics_data["total_number_of_real_commits"]
+        metrics_data["ratio_last_day_commits"]=counter_last_day_commits/metrics_data["total_number_of_real_commits"]
 
-        
-        metrics_data["same_day_concentration_ratio"]=0
-        metrics_data["consistency_score"]=0
+
+        if  metrics_data["total_number_of_real_commits"] == 0:
+            metrics_data["max_day_commits_ratio"] = 0
+        else:
+            max_commits_one_day = max(commits_per_day.values(), default=0)
+            metrics_data["max_day_commits_ratio"] = (
+                max_commits_one_day / metrics_data["total_number_of_real_commits"]
+            )
+
+
+        #trying to evaluate the distribution of the activity over the days register in commit history:
+        if metrics_data["total_number_of_real_commits"]<=1:
+            metrics_data["activity_distribution_ratio"]=0
+        elif metrics_data["activity_span_days"]==0:   
+            metrics_data["activity_distribution_ratio"]=0
+        else:
+            metrics_data["activity_distribution_ratio"]=metrics_data["active_days"]/(metrics_data["activity_span_days"]+1)
 
         metrics_resume={
             "repo": repo,
@@ -88,15 +126,69 @@ def get_commits_metrics(github_username:str,
             "metrics": metrics_data 
         }
 
+        #extra info about dates:
+        metadata_dates = {}
+
+        metadata_dates["first_commit_date"] = first_commit_date
+        metadata_dates["last_commit_date"] = last_commit_date 
+        metadata_dates["number_of_days_started_before_deadline"] = (deadline_data_ajustata-first_commit_date).days
+
+        #flags:
+        flags=[]
+        #low commit activity
+        if metrics_data["total_number_of_real_commits"]<3:
+            flag_low_commit_activity={
+                "name":"LOW_ACTIVITY_DISTRIBUTION",
+                "description":f"The student had only {metrics_data["total_number_of_real_commits"]} real commits (excluding merge ones)."
+            }
+            flags.append(flag_low_commit_activity)
+        
+        #flag last minute activity:
+        if metrics_data["ratio_last_day_commits"] > 0.5:
+            flag_last_minute_activity={
+                "name":"LAST_MINUTE_ACTIVITY",
+                "description":f"The student had {metrics_data["ratio_last_day_commits"]*100}% of commits done in last day"
+            }
+            flags.append(flag_last_minute_activity)
+
+        #flag HIGH_SAME_DAY_CONCENTRATION:
+        if metrics_data["max_day_commits_ratio"] > 0.6:
+            flag_high_same_day_activity={
+                "name":"HIGH_SAME_DAY_CONCENTRATION",
+                "description":f"The student had {metrics_data["max_day_commits_ratio"]*100}% of commits done in one day"
+            }
+            flags.append(flag_high_same_day_activity)
+
+        #flag LONG_INACTIVITY_GAP:
+        if metrics_data["largest_inactivity_gap_days"] >= 7:
+            flag_long_inactivity_gap={
+                "name":"LONG_INACTIVITY_GAP",
+                "description":f"The student had a big break from commiting, consisting of {metrics_data["largest_inactivity_gap_days"]} days without work."
+            }
+            flags.append(flag_long_inactivity_gap)
+
+        #flag LATE_START_PATTERN
+        if metadata_dates["number_of_days_started_before_deadline"] < 3:
+            flag_late_start={
+                "name":"LATE_START_PATTERN",
+                "description":f"The student have started working on this project {metrics_data["largest_inactivity_gap_days"]} days before deadline"
+            }
+            flags.append(flag_late_start)
+
         return{
             "status":"success",
-            "commits_analysis_resume":metrics_resume
+            "commits_analysis_resume":metrics_resume,
+            "flags":flags,
+            "metadata_dates":metadata_dates,
         }
     except Exception as e:
         return {
             "status": "failed",
             "error": str(e)
         }
+    
+
+
 #health check
 @router.get("/test3")
 def test():
